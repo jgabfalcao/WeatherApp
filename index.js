78b33043fe97167585f6351d11b2001fb02e69fb
João Falcão <jgabfalcao@gmail.com>
2023-05-21 13:31:51 -0300

    feat: condições para exibição do icone do clima

--- index.js
+++ index.js
@@ -33,6 +33,32 @@ search.addEventListener('click', () =>{
       const humidity = document.querySelector('weather-details .humidity span');
       const wind = document.querySelector('.weather-details .wind span');
 
+      switch (json.weather[0].main){
+        case 'Clear':
+          image.src = 'images/clear.png';
+          break;
+        
+        case 'Rain':
+          image.src = 'images/rain.png';
+          break;
+      
+        case 'Snow':
+          image.src = 'images/snow.png';
+          break;
+        
+        case 'Clouds':
+          image.src = 'images/cloud.png';
+          break;
+
+        case 'Haze':
+          image.src = 'images/mist.png';
+          break;
+
+        default:
+          image.src = '';
+      }
+      
+
       
     })
 })

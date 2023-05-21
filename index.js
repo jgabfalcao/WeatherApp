8033d0200c22eafd9e52372e2f99dc51f3d5303b
João Falcão <jgabfalcao@gmail.com>
2023-05-21 13:35:46 -0300

    feat: inserindo via js o dado que vem da api

--- index.js
+++ index.js
@@ -58,7 +58,11 @@ search.addEventListener('click', () =>{
           image.src = '';
       }
       
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>ºC</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
       
-    })
-})
+    });
+});
